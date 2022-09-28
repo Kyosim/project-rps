0324dc26159e7ac6b63fb69f6eb9f69883a2a086
Kevin Kim <kevin_kim@hotmail.ca>
2022-09-28 19:07:10 -0400

    Clean up playRound and playGame functions

--- javascript.js
+++ javascript.js
@@ -14,25 +14,37 @@ function getComputerChoice() {
 function playRound() {
     let playerChoice = prompt("Please choose one of rock, paper or scissors:" ).toLowerCase();
     let computerChoice = getComputerChoice();
-    if (playerChoice == "rock" && computerChoice == "paper") 
-        return result = "You lose! Paper beats Rock"
-    else if (playerChoice == "paper" && computerChoice == "rock") 
-        return result = "You win! Paper beats rock"
-    else if (playerChoice == "scissors" && computerChoice == "rock")
-        return result = "You lose! Rock beats scissors"
-    else if (playerChoice == "rock" && computerChoice == "scissors")
-        return result = "You win! Rock beats scissors"
-    else if (playerChoice == "paper" && computerChoice == "scissors")
-        return result = "You lose! Scissors beats paper"
-    else if (playerChoice == "scissors" && computerChoice == "paper") 
-        return result = "You win! Scissors beats paper"
-    else if (playerChoice == computerChoice) 
-        return result = `You both played ${computerChoice}.`
+    if ((playerChoice == "rock" && computerChoice == "paper") || (playerChoice == "scissors" && computerChoice == "rock") || (playerChoice == "paper" && computerChoice == "scissors")) 
+        return resultLose = `You lose; ${computerChoice} beats ${playerChoice}.`
+    else if ((playerChoice == "paper" && computerChoice == "rock") || (playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "scissors" && computerChoice == "paper")) 
+        return resultWin = `You win; ${playerChoice} beats ${computerChoice}.`
+    else 
+    return result = `You both played ${computerChoice}.`
 }
+
+// (playerChoice == "paper" && computerChoice == "rock") || (playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "scissors" && computerChoice == "paper")
+// (playerChoice == "rock" && computerChoice == "paper") || (playerChoice == "scissors" && computerChoice == "rock") || (playerChoice == "paper" && computerChoice == "scissors")
 
 function playGame () {
     for (let i = 0; i < 5; i++) {
         playRound();
         alert(result);
+        let playerTrack = 0;
+        let computerTrack = 0;
+        if (result = `You win; ${playerChoice} beats ${computerChoice}.`) {
+            playerTrack++;
+        }
+        else if (result = `You lose; ${computerChoice} beats ${playerChoice}.`) {
+            computerTrack++;
+        }
+    }
+    if (playerTrack > computerTrack) {
+        return "You win!"
+    }
+    else if (playerTrack < computerTrack) {
+        return "Too bad! You lose."
+    }
+    else {
+        return "It's a tie!"
     }
 }
