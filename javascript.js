7fadc7ef014e6c7feb4d410302cc7ff4b58b06d3
Kevin Kim <kevin_kim@hotmail.ca>
2022-09-28 18:26:28 -0400

    Make function that plays one round

--- javascript.js
+++ javascript.js
@@ -11,15 +11,27 @@ function getComputerChoice() {
         return "scissors"
     }
 } 
-
-function getPlayerChoice() {
-    let playerChoice = prompt("Please play one of rock, paper, or scissors:" )
-    let playerChoiceCorrected = playerChoice.toLowerCase();
-    return playerChoiceCorrected;
+function playRound() {
+    let playerChoice = prompt("Please choose one of rock, paper or scissors:" ).toLowerCase();
+    let computerChoice = getComputerChoice();
+    if (playerChoice == "rock" && computerChoice == "paper") 
+        return "You lose! Paper beats Rock"
+    else if (playerChoice == "paper" && computerChoice == "rock") 
+        return "You win! Paper beats rock"
+    else if (playerChoice == "scissors" && computerChoice == "rock")
+        return "You lose! Rock beats scissors"
+    else if (playerChoice == "rock" && computerChoice == "scissors")
+        return "You win! Rock beats scissors"
+    else if (playerChoice == "paper" && computerChoice == "scissors")
+        return "You lose! Scissors beats paper"
+    else if (playerChoice == "scissors" && computerChoice == "paper") 
+        return "You win! Scissors beats paper"
+    else if (playerChoice == computerChoice) 
+        return `You both played ${computerChoice}.`
 }
 
-function playOneRound(playerSelection, compSelection) {
-    let playerSelection = getPlayerChoice();
-    let compSelection = getComputerChoice();
+function playGame () {
+    for (let i = 0; i < 5; i++) {
+        
+    }
 }
-
