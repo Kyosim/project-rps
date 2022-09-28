1e7b514db229e44fea195204a6e607dd4ac4baad
Kevin Kim <kevin_kim@hotmail.ca>
2022-09-28 18:37:16 -0400

    Add 5 round RPS function

--- javascript.js
+++ javascript.js
@@ -15,23 +15,24 @@ function playRound() {
     let playerChoice = prompt("Please choose one of rock, paper or scissors:" ).toLowerCase();
     let computerChoice = getComputerChoice();
     if (playerChoice == "rock" && computerChoice == "paper") 
-        return "You lose! Paper beats Rock"
+        return result = "You lose! Paper beats Rock"
     else if (playerChoice == "paper" && computerChoice == "rock") 
-        return "You win! Paper beats rock"
+        return result = "You win! Paper beats rock"
     else if (playerChoice == "scissors" && computerChoice == "rock")
-        return "You lose! Rock beats scissors"
+        return result = "You lose! Rock beats scissors"
     else if (playerChoice == "rock" && computerChoice == "scissors")
-        return "You win! Rock beats scissors"
+        return result = "You win! Rock beats scissors"
     else if (playerChoice == "paper" && computerChoice == "scissors")
-        return "You lose! Scissors beats paper"
+        return result = "You lose! Scissors beats paper"
     else if (playerChoice == "scissors" && computerChoice == "paper") 
-        return "You win! Scissors beats paper"
+        return result = "You win! Scissors beats paper"
     else if (playerChoice == computerChoice) 
-        return `You both played ${computerChoice}.`
+        return result = `You both played ${computerChoice}.`
 }
 
 function playGame () {
     for (let i = 0; i < 5; i++) {
-        
+        playRound();
+        alert(result);
     }
 }
